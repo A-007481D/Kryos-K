47c66b348146cdeef61e640ec9284bcc1439431b
A-007481D <labidabdelmalek@gmail.com>
2026-09-16 03:07:50 +0100

    lib: add freestanding memset/memcpy/strlen implementation
    Add kernel/lib/string.c with freestanding implementations of:
    - memset: simple byte-fill loop
    - memcpy: simple byte-copy loop
    - strlen: null-terminated length
    
    These are required by the ELF loader (BSS zeroing, segment copy)
    and test infrastructure. Added include/string.h header and
    string.o to Makefile OBJS.

---
+++ kernel/lib/string.c
@@ -0,0 +1,24 @@
+#include "../../include/string.h"
+
+void *memset(void *s, int c, size_t n) {
+    unsigned char *p = s;
+    while (n--) {
+        *p++ = (unsigned char)c;
+    }
+    return s;
+}
+
+void *memcpy(void *dest, const void *src, size_t n) {
+    char *d = dest;
+    const char *s = src;
+    while (n--) {
+        *d++ = *s++;
+    }
+    return dest;
+}
+
+size_t strlen(const char *s) {
+    size_t len = 0;
+    while (*s++) len++;
+    return len;
+}
